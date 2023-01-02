7f19204e87367df6d94f32b139194a4baeb75221
Cipppp <pirvuciprian37@gmail.com>
2023-01-02 22:55:49 +0200

    Migrated from normal C++ code to QT GUI library

---
+++ mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QMessageBox>
+#include <QPixmap>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPixmap pix(":/resources/img/logo.png");
+    int width = ui->pictureLabel->width();
+    int height = ui->pictureLabel->height();
+    ui->pictureLabel->setPixmap(pix.scaled(width, height, Qt::KeepAspectRatio));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_Login_clicked()
+{
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_password->text();
+
+    if (username == "admin" && password == "admin")
+    {
+        QMessageBox::information(this, "Login", "Username and password is correct");
+        hide();
+        secDialog = new SecDialog(this);
+        secDialog->show();
+    }
+    else
+    {
+        QMessageBox::warning(this, "Login", "Username and password is not correct");
+    }
+}
+
+
